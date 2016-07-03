542ccbf2aec367183e68d48f71d783ea14902ad8
Matthew <hallm241@users.noreply.github.com>
2016-07-03 21:59:45 +1000

    Create download script for data file (download to temp, unzip and read table, delete temp).

---
+++ Dataprep.R
@@ -0,0 +1,7 @@
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data <- read.table(unz(temp, "household_power_consumption.txt"), header = TRUE, sep = ";", na.strings = "?")
+unlink(temp)
+
+str(data)
+names(data)
